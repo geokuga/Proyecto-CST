a57fdf7b22401a24dc864c1883b5c5f61364148b
Brian <za220111239@zapopan.tecmm.edu.mx>
2024-12-23 09:17:16 -0600

    primera presentacion

--- src/components/HeaderHome/HeaderHome.tsx
+++ src/components/HeaderHome/HeaderHome.tsx
@@ -5,7 +5,12 @@ export default function HeaderHome() {
     <section className="banner">
       <section className="HContent">
         <h1>CAJA SOLIDARIA TALA</h1>
-        <p>La mejor forma de guardar tus ahorros</p>
+        <p>Conviertete en socio para obtener los mejores beneficios</p>
+        <div>
+          <button className="HButton" type="button">
+            Ser socio
+          </button>
+        </div>
       </section>
     </section>
   );
