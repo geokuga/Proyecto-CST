364ae60d90bdcb81f985eb0f0a5a737b8fcc70b5
Brian <za220111239@zapopan.tecmm.edu.mx>
2024-12-27 19:59:56 -0600

    actualizacion de requisitos

--- src/components/Requisitos/Requisitos.tsx
+++ src/components/Requisitos/Requisitos.tsx
@@ -8,11 +8,9 @@ export default function Requisitos() {
           <h3>Ser socio</h3>
         </div>
         <div className="TextoContent">
-          <p>Acta de nacimiento</p>
-          <p>CURP</p>
           <p>Comprobante de domicilio</p>
           <p>Identificacion Oficial</p>
-          <p>Acta de matrimonio (en caso de contar con ella)</p>
+          <p>Parte social ($960)</p>
         </div>
       </section>
       <section className="RequisitosContent">
@@ -27,7 +25,30 @@ export default function Requisitos() {
         </div>
       </section>
       <section className="RequisitosContent">
-        <div className="TituloContent"></div>
+        <div className="TituloContent">
+          <h3>Creditos</h3>
+        </div>
+        <div className="TextoContent">
+          <p>
+            <b>Socio</b>
+          </p>
+          <p>Comprobante de domicilio</p>
+          <p>Identificacion oficial</p>
+          <p>Comprobante de ingresos</p>
+          <p>Tener ahorro de 10% o 20%</p>
+          <p>
+            <b>Aval</b>
+          </p>
+          <p>Comprobante de domicilio</p>
+          <p>Identificacion oficial</p>
+          <p>Comprobante de ingresos</p>
+          <p>No ser mayor de 60 años de edad</p>
+        </div>
+      </section>
+      <section className="RequisitosContent">
+        <div className="TituloContent">
+          <h3>Inversiones</h3>
+        </div>
         <div className="TextoContent"></div>
       </section>
     </section>
--- src/components/publicidad/publicidad.tsx
+++ src/components/publicidad/publicidad.tsx
@@ -4,9 +4,9 @@ export default function publicidad() {
   return (
     <section className="PContent">
       <section className="Pcontentenedor">
-        <img className="imagen" src="public\logos\caña.png" alt="" />
         <img className="imagen" src="public\img\publicidad1.jpeg" alt="" />
-        <img className="imagen" src="public\logos\caña.png" alt="" />
+        <img className="imagen" src="public\img\publicidad1.jpeg" alt="" />
+        <img className="imagen" src="public\img\publicidad1.jpeg" alt="" />
         <img className="imagen" src="public\img\publicidad1.jpeg" alt="" />
       </section>
     </section>
