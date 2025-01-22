208fa2de9788704cd5527a166d6c6ef7de944c86
Brian <za220111239@zapopan.tecmm.edu.mx>
2025-01-22 17:00:21 -0600

    actualizacion header

--- src/components/HeaderHome/HeaderHome.tsx
+++ src/components/HeaderHome/HeaderHome.tsx
@@ -1,5 +1,16 @@
 import "./HeaderHome.css";
 
 export default function HeaderHome() {
-  return <section className="banner"></section>;
+  return (
+    <section className="banner">
+      <section className="HContent">
+        <h3>Caja Solidaria Tala</h3>
+        <p>La caja de nuestra gente</p>
+        <center>
+          <button className="HeaderBton"> Ser socio</button>
+        </center>
+      </section>
+      <img src="public\logos\caña.png"></img>
+    </section>
+  );
 }
--- src/components/MisionVision/MisionVision.tsx
+++ src/components/MisionVision/MisionVision.tsx
@@ -12,7 +12,7 @@ export default function MisionVision() {
           Somos una sociedad cooperativa de ahorro y préstamo autorizada, que
           opera en la región Valles; brindando educación financiera, promoviendo
           el ahorro y otorgando créditos accesibles a las necesidades de los
-          socios, para elevar su nivel de vida.
+          socios para elevar su nivel de vida.
         </p>
       </section>
 
@@ -22,10 +22,10 @@ export default function MisionVision() {
           <i className="fa fa-eye iconoM" aria-hidden="true"></i>
         </center>
         <p className="MTexto">
-          Ser una sociedad cooperativa de ahorro y préstamo de nivel dos, lograr
+          Ser una sociedad cooperativa de ahorro y préstamo de nivel dos; lograr
           y mantener una alta calidad en nuestros productos y servicios con
-          tasas de interés activas y pasivas competitivas, incursionar en otros
-          segmentos del mercado en el cual operamos, para beneficio de nuestros
+          tasas de interés activas y pasivas competitivas; incursionar en otros
+          segmentos del mercado en el cual operamos para beneficio de nuestros
           asociados y población en general.
         </p>
       </section>
