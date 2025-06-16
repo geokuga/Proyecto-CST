78a535e2e1f8a4456ac9d1c146c4225bf15dbd54
Brian <za220111239@zapopan.tecmm.edu.mx>
2025-06-16 15:15:36 -0600

    Integracion de iconos en HOME y nuevo componente descripcion en water

--- src/components/PaginaHome/EnlaceIcons/EnlaceIcons.tsx
+++ src/components/PaginaHome/EnlaceIcons/EnlaceIcons.tsx
@@ -1,4 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDroplet, faGift, faShop } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import "./EnlaceIcons.css";
+
 export default function EnlaceIcons() {
-  return <div>EnlaceIcons</div>;
+  return (
+    <section className="enlace-icons-container">
+      <section className="enlace-icons-section">
+        <Link to="/promociones">
+          <div className="enlace-icons icon-gift">
+            <FontAwesomeIcon icon={faGift} />
+          </div>
+          <h1>Promociones</h1>
+        </Link>
+      </section>
+
+      <section className="enlace-icons-section">
+        <Link to="/water">
+          <div className="enlace-icons icon-water">
+            <FontAwesomeIcon icon={faDroplet} />
+          </div>
+          <h1>Proyecto Water</h1>
+        </Link>
+      </section>
+
+      <section className="enlace-icons-section">
+        <Link to="/otrosBeneficios">
+          <div className="enlace-icons icon-shop">
+            <FontAwesomeIcon icon={faShop} />
+          </div>
+          <h1>Otros servicios</h1>
+        </Link>
+      </section>
+    </section>
+  );
 }
---
+++ src/components/Water/DescripcionWater/Descripcion.tsx
@@ -0,0 +1,20 @@
+import "./Descripcion.css";
+
+export default function Descripcion() {
+  return (
+    <section className="DContenedor">
+      <section className="DContent">
+        <div className="DTitulo">
+          <h3>¿Quiénes somos?</h3>
+        </div>
+        <div className="DTexto">
+          En nuestra institución financiera, estamos comprometidos con tu éxito
+          y bienestar. Ofrecemos una amplia gama de servicios personalizados
+          para ayudarte a alcanzar tus metas financieras, desde ahorros e
+          inversiones hasta préstamos y asesoramiento financiero.
+        </div>
+      </section>
+      <section className="DContentImagen"></section>
+    </section>
+  );
+}
