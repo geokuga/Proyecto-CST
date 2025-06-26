0e77594cac4cee83db77e42afa1e4d899d6c26bc
Brian <za220111239@zapopan.tecmm.edu.mx>
2025-06-26 12:32:13 -0600

    terminar pagina de promocion

--- src/components/Promociones/Promociones.tsx
+++ src/components/Promociones/Promociones.tsx
@@ -1,21 +1,28 @@
 import Carrusel from "./CarrouselRegalos/CarrouselRegalos";
+import Descripcion from "./DescripcionPromociones/DescripcionPromociones";
+import Footer from "../PaginaHome/footer/footer";
 import "./Promociones.css";
 
 export default function CarrouselRegalos() {
   const imagenesRegalos = [
-    "logos/caña.png",
-    "https://m.media-amazon.com/images/I/81QjxylSV4L.jpg",
-    "https://cdn.homedepot.com.mx/productos/170791/170791-a3.jpg",
-    "https://takka.mx/wp-content/uploads/2024/11/PAR-027-A.jpg",
-    "https://termosleon.com.mx/wp-content/uploads/2024/02/termos-personalizados-con-nombre-azul.jpg",
-    "https://m.media-amazon.com/images/I/61KMIdgw1+L.jpg",
-    "https://m.media-amazon.com/images/I/81QjxylSV4L.jpg",
-    "https://cdn.homedepot.com.mx/productos/170791/170791-a3.jpg",
+    "img/Promocion1.png",
+    "img/Promocion2.png",
+    "img/Promocion3.png",
+    "img/Promocion4.png",
+    "img/Promocion5.png",
   ];
 
   return (
-    <section className="carrousel-regalos">
-      <Carrusel imageList={imagenesRegalos} />
+    <section>
+      <div className="carrousel-regalos">
+        <Carrusel imageList={imagenesRegalos} />
+      </div>
+      <div>
+        <Descripcion />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </section>
   );
 }
